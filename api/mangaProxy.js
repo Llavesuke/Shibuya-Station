b18527ed37cf285980cec5a1a2e2f3a55e083b9f
Llavesuke <llavesukeprogram@gmail.com>
2024-12-08 06:59:06 +0100

    fix 13

---
+++ api/mangaProxy.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+const baseUrl = 'https://api.mangadex.org';
+
+export default async function handler(req, res) {
+  const { method, query } = req;
+
+  if (method === 'GET' && query.imageUrl) {
+    try {
+      const { imageUrl } = query;
+      const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+      const imageBuffer = Buffer.from(response.data, 'binary').toString('base64');
+      res.status(200).json({ image: imageBuffer });
+    } catch (error) {
+      console.error('Error fetching image:', error);
+      res.status(500).json({ error: 'Error fetching image' });
+    }
+  } else if (method === 'GET' && !query.imageUrl) {
+    try {
+      // Aquí redirigimos la solicitud de manga a la API de MangaDex.
+      const response = await axios.get(`${baseUrl}/manga`, { params: req.query });
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.status(200).json(response.data);
+    } catch (error) {
+      console.error('Error fetching mangas:', error);
+      res.status(500).json({ error: 'Error fetching mangas' });
+    }
+  }
+}
